6d4ee266d915cb1d97b4b413eacf05443bf4b54a
Nelson Tapia <ntapiapinto@gmail.com>
2023-08-30 15:18:00 -0400

    Initial Commit

---
+++ Sec19-Json-URLSession/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  Sec19-Json-URLSession
+//
+//  Created by nelson tapia on 30-08-23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Sec19-Json-URLSession/Sec19_Json_URLSessionApp.swift
@@ -0,0 +1,17 @@
+//
+//  Sec19_Json_URLSessionApp.swift
+//  Sec19-Json-URLSession
+//
+//  Created by nelson tapia on 30-08-23.
+//
+
+import SwiftUI
+
+@main
+struct Sec19_Json_URLSessionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
